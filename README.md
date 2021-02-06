ea58544f63c27d76696d67ddd0afe10a5f9a0c02
helewrer3 <helewrer3@gmail.com>
2021-02-06 19:37:43 +0530

    Debugging

---
+++ README.md
@@ -0,0 +1,3 @@
+# Helewcial v2
+A new and improved version of [Helewcial](https://github.com/helewrer3/Social-Media-App)!
+### Deployed to [Heroku](https://helewcialv2.herokuapp.com/)!
--- helpers/hbs.js
+++ helpers/hbs.js
@@ -9,7 +9,7 @@ module.exports = {
         str = str.substr(0, str.length - 3)
         if(str.length > len) str = str.substr(0, len) + '...'
         str = '<p>' + str
-        str = str + '</p>'
+        str = str + '/p>'
         console.log(str);
         return str
     }
